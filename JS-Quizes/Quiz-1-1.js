5dd11094b18fc32a88dff052b3d939721d536b19
Sheyma Fathy <wbdesigner249@gmail.com>
2018-09-27 20:43:08 +0200

    Create Quiz-1-1.js

---
+++ JS-Quizes/Quiz-1-1.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: First Expression (1-1)
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// type yout code below
+
+
+
+console.log(20 * 2 + 5 - 3);
