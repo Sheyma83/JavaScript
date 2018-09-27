ed5f5248a106ea50bad3fe6dfc08ee84c360fb09
Sheyma Fathy <wbdesigner249@gmail.com>
2018-09-27 20:56:45 +0200

    Create Quiz-1-6.js

---
+++ JS-Quizes/Quiz-1-6.js
@@ -0,0 +1,11 @@
+What character will be printed to the JavaScript console after running the following lines of code.
+‭var quote = "Stay awhile and listen!";‬
+‭console.log(quote[6]);‬
+
+hint: this lesson is about string indexing.
+
+
+‭var quote = "Stay awhile and listen!";‬
+‭console.log(quote[6]);‬
+
+Returns: "w"
