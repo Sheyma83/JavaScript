52f6db182c24adf730c791b54cdcd17e9da181ea
Sheyma Fathy <wbdesigner249@gmail.com>
2018-09-27 21:25:44 +0200

    Add files via upload

---
+++ JS-Quizes/Quiz-1-7.js
@@ -0,0 +1,22 @@
+QUIZ ‫Description‬
+Select the string that returns the following output:
+‭"The file located at "C:\\Desktop\My Documents\Roster\names.txt" contains the names on the roster."‬
+		 ‭"The file located at "C:\\Desktop\My Documents\Roster\names.txt" contains the names on the roster."‬   
+		 ‭"The file located at \"C:\\Desktop\My Documents\Roster\names.txt\" contains the names on the roster."‬   
+		‭"The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster."‬
+
+
+
+hint : escaping characters is 
+
+\\
+\ (backslash)
+\"
+'' (double quote)
+\'
+' (single quote)
+\n
+newline
+\t
+tab
+
